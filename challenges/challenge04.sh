d4c0e8cd14cffabb39255402bb8cffa40f2d10de
unoffensive <barnettlaurel73@gmail.com>
2024-06-08 01:03:09 +0500

    updated challenge description

---
+++ challenges/challenge04.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Define the animation frames
+frames=("|" "/" "-" "\\")
+
+# Number of frames
+num_frames=${#frames[@]}
+
+# Duration to run the animation (in seconds)
+duration=3
+
+# Calculate the end time
+end_time=$((SECONDS + duration))
+
+# Clear the line
+clear_line="\r\033[K"
+
+echo "Loading..."
+
+# Loop until the time is up
+while [ $SECONDS -lt $end_time ]; do
+    for ((i=0; i<num_frames; i++)); do
+        # Print the frame
+        echo -ne "${frames[i]}"
+        
+        # Delay for a short time (0.1 seconds)
+        sleep 0.1
+        
+        # Clear the line and move back to the start
+        echo -ne "$clear_line"
+    done
+done
+
+echo "Your Description Loading Please Wait..."
+
+frames=("|" "/" "-" "\\")
+num_frames=${#frames[@]}
+duration=3
+end_time=$((SECONDS + duration))
+clear_line="\r\033[K"
+
+# Loop until the time is up
+while [ $SECONDS -lt $end_time ]; do
+    for ((i=0; i<num_frames; i++)); do
+        # Print the frame
+        echo -ne "${frames[i]}"
+        
+        # Delay for a short time (0.1 seconds)
+        sleep 0.1
+        
+        # Clear the line and move back to the start
+        echo -ne "$clear_line"
+    done
+done
+
+echo "Find the Hidden Flag by Searching Through Hidden Files and Temps, Do you Remember This Game Bruh)))"
